dddd34cd2bace606dbef8409b2eac54d5dde966f
DrathJ <juliusdrath@yahoo.de>
2022-09-24 20:29:34 +0200

    Created classes

---
+++ Test2/src/Uni.cpp
@@ -0,0 +1,10 @@
+//
+// Created by juliu on 24.09.2022.
+//
+
+#include "Uni.h"
+
+
+Uni::Uni() {
+    std::cout <<"Hello University" << std::endl;
+}
---
+++ Test2/src/Uni.h
@@ -0,0 +1,24 @@
+//
+// Created by juliu on 24.09.2022.
+//
+
+#ifndef SEMESTER_2_UNI_H
+#define SEMESTER_2_UNI_H
+
+#include <iostream>
+
+
+class Uni {
+
+
+public:
+    Uni();
+
+private:
+
+
+
+};
+
+
+#endif //SEMESTER_2_UNI_H
