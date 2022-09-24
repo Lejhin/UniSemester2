6155567f324e5b7af3bd162810e0b8a761fc6651
DrathJ <juliusdrath@yahoo.de>
2022-09-24 20:17:03 +0200

    Added Test1 and Test2

---
+++ Test1/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by juliu on 24.09.2022.
+//
+#include <iostream>
+
+
+int main(){
+
+    std::cout << "Hello World" << std::endl;
+
+}
---
+++ Test2/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by juliu on 24.09.2022.
+//
+#include <iostream>
+
+int main(){
+    std::cout << "No Homo" << std::endl;
+}
