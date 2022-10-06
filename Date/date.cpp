112fec811434c91f896ad01e6b88cb21e88a4088
Lejhin <juliusdrath@yahoo.de>
2022-10-06 10:44:59 +0200

    deleted old print

--- Date/date.cpp
+++ Date/date.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 #include "date.h"
 
- void print(date* d){
-
-    std::cout << d << std::endl;
-}
-
-
-static int m30[4] {4,6,9,11};
-static int m31[7] {1,3,5,7,8,10,12};
 
 date::date( int day, int month, int year){
     this->day = day;
@@ -21,7 +13,7 @@ void date::add_day(int day){
     this->day += day;
 }
 
-void date::print(){
+void date::print() const{
     std::cout << this->day << "." << this->month << "." << this->year << std::endl;
 }
 
