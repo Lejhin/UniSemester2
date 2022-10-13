b3c94b5130fb638dbf8c0deb0d224d38b7cc7272
Lejhin <juliusdrath@yahoo.de>
2022-10-13 10:51:37 +0200

    added copy&swap funktion in String

--- String/String.h
+++ String/String.h
@@ -12,11 +12,18 @@ class String{
 
         char* str;
         size_t length;
+        void deepCopy(const String& newString){
+                
+                this->length = newString.length;
+                this->str = new char[this->length+1];
+                strcpy(str, newString.str);
+        }
 
     public:
 
-        String(const char* String) : str(new char +1){
+        String(const char* String) {
             length = strlen(String);
+            str = new char[length+1];
             strcpy(str, String);
         }
 
@@ -26,10 +33,27 @@ class String{
 
         //Kopierkonstruktor
         String(const String& s){
-            length = s.length;
-            str = new char[length+1];
-            strcpy(str, s.str);   
+            deepCopy(s);
         }
+
+        String& operator=(const String& newString){
+            if(this->str != newString.str){
+                delete[] this->str;
+                deepCopy(newString);
+            }
+            return *this;
+        }
+
+        /* Vorteil : Funktioniert immer!!
+        
+        String& operator=(const String& newString){
+            String local(newString);
+            std::swap(length, local.length);
+            std::swap(s. local.str);
+            return *this;
+        }
+        */
+
         void print(){
             std::cout << str << std::endl;
         }
